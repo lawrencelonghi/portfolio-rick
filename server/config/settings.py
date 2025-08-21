ec4cb4444ec78ce7d85318afd6885071d9158d20
lawrencelonghi <lawlonghi@gmail.com>
2025-08-21 12:03:40 -0300

    more settings changes about statifile and static root because getting 200 in the site but static files fails and server/dockerfile changes too

--- server/config/settings.py
+++ server/config/settings.py
@@ -136,10 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = []  # No need to point to client/dist
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "client/dist/assets"),
-]
 
 
 # Default primary key field type
