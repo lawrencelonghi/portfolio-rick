1ad7fafebe8dcf8c2b5a4ad34845e41f08d30cc6
lawrencelonghi <lawlonghi@gmail.com>
2025-08-22 10:05:49 -0300

    urls re_path

--- server/config/urls.py
+++ server/config/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -27,5 +28,6 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
